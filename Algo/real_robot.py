0a92c841e01a36112a45a8088d5dc6520d5dcf32
Anqi Tu <anqitu@outlook.com>
2019-03-14 12:51:01 +0800

    Reset calibration distance

--- Algo/real_robot.py
+++ Algo/real_robot.py
@@ -411,7 +411,7 @@ class Robot:
         cells_right = []
 
         if sensor_facing == WEST:
-            for distance in range(2, 5):
+            for distance in range(2, 4):
                 new_x = x - distance
                 if new_x < 0:
                     cells_left.append(1)
@@ -420,7 +420,7 @@ class Robot:
                     cells_left.append(discovered_map[y - 1][new_x])
                     cells_right.append(discovered_map[y + 1][new_x])
         elif sensor_facing == NORTH:
-            for distance in range(2, 5):
+            for distance in range(2, 4):
                 new_y = y + distance
                 if new_y > 19:
                     cells_left.append(1)
@@ -429,7 +429,7 @@ class Robot:
                     cells_left.append(discovered_map[new_y][x - 1])
                     cells_right.append(discovered_map[new_y][x + 1])
         elif sensor_facing == EAST:
-            for distance in range(2, 5):
+            for distance in range(2, 4):
                 new_x = x + distance
                 if new_x > 14:
                     cells_left.append(1)
@@ -438,7 +438,7 @@ class Robot:
                     cells_left.append(discovered_map[y + 1][new_x])
                     cells_right.append(discovered_map[y - 1][new_x])
         elif sensor_facing == SOUTH:
-            for distance in range(2, 5):
+            for distance in range(2, 4):
                 new_y = y - distance
                 if new_y < 0:
                     cells_left.append(1)
